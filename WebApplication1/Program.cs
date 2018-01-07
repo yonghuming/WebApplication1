f51b0f1a222323b74a811d077986200d401c10cd
pydo <964683112@qq.com>
2018-01-07 21:03:42 +0800

    外网可以访问

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -19,6 +19,7 @@ namespace WebApplication1
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseUrls("http://*:5000")
                 .UseStartup<Startup>()
                 .Build();
     }
